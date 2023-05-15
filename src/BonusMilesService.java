7a62bd02fa9456cb93ee25c010ade8cdebb70685
Alexandr Vinogradov <vinograder@yandex.ru>
2023-05-15 23:00:15 +0300

    Solve

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,14 +1,7 @@
 public class BonusMilesService {
 
-    public <b1, BonusReward> int calculate(int miles) {
-        boolean b = miles < 1;
-        boolean b1 = miles > 1;
-        int BonusReward;
-        if (b) {
-            BonusReward = 20;
-        } else {
-            BonusReward = 0;
-        }
+    public int calculate(int price) {
+        int miles = price / 20;
         return miles;
     }
 }
